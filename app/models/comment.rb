4fe80b207cfa2b49160ca3adfe5fcaa9f2fb2ac6
JSG <wlsh.jsg@gmail.com>
2017-06-23 22:35:22 +0800

    Update models
    - Add user_id column
    - Add admin role to user model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :restaurant
+  belongs_to :user
 
   validates_inclusion_of :rating, in: 1..5
   validates :content, length: { in: 0..20 }, allow_nil: true
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623123457) do
+ActiveRecord::Schema.define(version: 20170623142328) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "restaurant_id"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 20170623123457) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170623123457) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
